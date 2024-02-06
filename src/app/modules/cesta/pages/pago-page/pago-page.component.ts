054f2601101e2e995a702fda6d1c8e464ed8c544
Iria <irialcban@alu.edu.gva.es>
2024-02-06 13:33:17 +0100

    Pago modificado

--- src/app/modules/cesta/pages/pago-page/pago-page.component.ts
+++ src/app/modules/cesta/pages/pago-page/pago-page.component.ts
@@ -4,13 +4,29 @@ import { SharedService } from 'src/app/shared/services/shared.service';
 @Component({
     selector: 'cesta-pago-page',
     templateUrl: './pago-page.component.html',
+    styles:  [`
+      .enlace {
+        text-decoration: none;
+        }
+    
+      .enlace:hover {
+        color: #ca8a04;
+      }
+    `],
 })
 export class PagoPageComponent implements OnInit {
-  ngOnInit(): void {}
-
   constructor(private sharedService: SharedService){}
+
+  realizada = false
 
   clearList() {
     this.sharedService.clearProducts();
+    this.realizada = false;
   }
+
+  compraRealizada() {
+    this.realizada = true;
+  }
+
+  ngOnInit(): void {}
 }
